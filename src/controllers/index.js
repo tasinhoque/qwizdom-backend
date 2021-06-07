a35d0a0ab4f8343cdd1d485d10cb7d1bf1ad825b
Tasin Hoque <tasinhoque@gmail.com>
2021-06-07 08:45:15 +0600

    Set up controllers and services

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,29 @@
 const authController = require('./auth');
 const userController = require('./user');
 const quizController = require('./quiz');
+const categoryController = require('./category');
+const commentController = require('./comment');
+const discussionThreadController = require('./discussionThread');
+const leaderboardController = require('./leaderboard');
+const notificationController = require('./notification');
+const questionController = require('./question');
+const questionResponseController = require('./questionResponse');
+const quizResponseController = require('./quizResponse');
+const reviewController = require('./review');
+const stageController = require('./stage');
 
 module.exports = {
   authController,
   userController,
   quizController,
+  categoryController,
+  commentController,
+  discussionThreadController,
+  leaderboardController,
+  notificationController,
+  questionController,
+  questionResponseController,
+  quizResponseController,
+  reviewController,
+  stageController,
 };
--- src/services/index.js
+++ src/services/index.js
@@ -3,6 +3,16 @@ const emailService = require('./email');
 const tokenService = require('./token');
 const userService = require('./user');
 const quizService = require('./quiz');
+const categoryService = require('./category');
+const commentService = require('./comment');
+const discussionThreadService = require('./discussionThread');
+const leaderboardService = require('./leaderboard');
+const notificationService = require('./notification');
+const questionService = require('./question');
+const questionResponseService = require('./questionResponse');
+const quizResponseService = require('./quizResponse');
+const reviewService = require('./review');
+const stageService = require('./stage');
 
 module.exports = {
   authService,
@@ -10,4 +20,14 @@ module.exports = {
   tokenService,
   userService,
   quizService,
+  categoryService,
+  commentService,
+  discussionThreadService,
+  leaderboardService,
+  notificationService,
+  questionService,
+  questionResponseService,
+  quizResponseService,
+  reviewService,
+  stageService,
 };
