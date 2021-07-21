ecf007aa484b123f5ff3b6a6b46ecf954e5bba88
Tasin Hoque <tasinhoque@gmail.com>
2021-07-21 11:08:34 +0600

    Populate

--- src/services/discussionThread.js
+++ src/services/discussionThread.js
@@ -5,7 +5,7 @@ const create = async body => DiscussionThread.create(body);
 const update = async (id, updateBody) =>
   DiscussionThread.findByIdAndUpdate(id, updateBody, { new: true }).orFail();
 
-const get = async id => DiscussionThread.findById(id).orFail();
+const get = async id => DiscussionThread.findById(id).populate('user').orFail();
 
 const getByQuiz = async (quizId, page, limit) =>
   DiscussionThread.paginate(
